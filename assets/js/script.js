ebe56899c5bea8e5ad3bab3b625d519246a87f4f
Matheus Gabriel <matheusg663@gmail.com>
2022-06-28 00:18:09 -0300

    Contador vai até 10

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,39 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
+var adicionar = document.getElementsByName('adicionar');
+var subtrair = document.getElementsByName('subtrair');
+
+adicionar[0].addEventListener("click", increment);
+subtrair[0].addEventListener("click", decrement);
+
+function verificarBtn() {
+    if((currentNumber + 1) > 10) {
+        document.getElementsByName("adicionar")[0].disabled = true;
+    } else {
+        document.getElementsByName("adicionar")[0].disabled = false;
+    }
+
+    if((currentNumber - 1) < 0) {
+        document.getElementsByName('subtrair')[0].disabled = true;
+    } else {
+        document.getElementsByName('subtrair')[0].disabled = false;
+    }
+}
+
 function increment() {
-    currentNumber++;
+    if(currentNumber < 10) {
+        currentNumber++;
+       verificarBtn();
+    }
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
-    currentNumber--;
+    if(currentNumber > 0) {
+        currentNumber--;
+        verificarBtn();
+    }
+    
     currentNumberWrapper.innerHTML = currentNumber;
 }
